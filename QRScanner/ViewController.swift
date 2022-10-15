aea512058fa8fffe0e536b03dd827e8fddb62d0d
MaadhavMohan <malleshahyalya@gmai.com>
2022-10-15 13:32:33 +0800

    Initial Commit

---
+++ QRScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by user227716 on 10/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
